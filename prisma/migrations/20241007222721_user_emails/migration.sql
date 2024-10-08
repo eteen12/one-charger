becc0d030e5b938bedc22c67f740413275cbd271
eteen <eteensmash@gmail.com>
2024-10-07 22:18:09 -0700

    switched from sqlite to postgres changed schema migrations and env

--- prisma/migrations/20241007222721_user_emails/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "userEntry" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "userEntry_email_key" ON "userEntry"("email");
