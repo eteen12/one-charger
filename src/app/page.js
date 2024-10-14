42c7fef561405438cb7e17d43cfa6dfbb1a037b7
eteen <eteensmash@gmail.com>
2024-10-14 09:41:15 -0700

    added productFeatures tailwind component

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,14 @@ import React from "react";
 import Hero from "../components/sections/hero";
 import Cta1 from "@/components/sections/cta1";
 import Faq from "@/components/sections/faq";
+import ProductFeatures from "@/components/sections/productFeatures";
 
 export default function Home() {
   return (
     <div className="z-40">
       <Hero />
       <Cta1 />
-
+      <ProductFeatures />
       <Faq />
     </div>
   );
