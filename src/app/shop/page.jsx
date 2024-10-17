1290bccf0746a38b0149e1b8c9c846f686801c83
eteen <eteensmash@gmail.com>
2024-10-16 20:28:40 -0700

    email page made and sumbissions work

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -31,10 +31,7 @@ export default function Page() {
   return (
     <div className="bg-gray-50">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-        {/* Product details */}
         <ProductStaticContent />
-
-        {/* Product image */}
         <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
           <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
             <Image
@@ -44,8 +41,6 @@ export default function Page() {
             />
           </div>
         </div>
-
-        {/* Product form */}
         <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
           <section aria-labelledby="options-heading">
             <h2 id="options-heading" className="sr-only">
@@ -54,7 +49,6 @@ export default function Page() {
 
             <form>
               <div className="sm:flex sm:justify-between">
-                {/* Amount selector */}
                 <fieldset>
                   <legend className="block text-sm ralewayBold">Amount</legend>
                   <RadioGroup
